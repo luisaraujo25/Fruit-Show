c059dead61cb65e4b6e284e443edfeed74cceb4f
Lu <lmpa.pt@gmail.com>
2022-12-11 17:50:54 +0100

    clicks

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ window.addEventListener('load', function(){
     const NO_FRUITS = 3;
     const MAX_FRUIT = 5;
     const SPEED = 2;
+    const FPS = 1000 / 25;
 
     const ctx = game.getContext("2d");
     
@@ -46,5 +47,52 @@ window.addEventListener('load', function(){
         angle += 2 * Math.PI / 180;
         requestAnimationFrame(move);
     }
-    move();
+
+    
+    let counter = 0;
+    const RESP = 10000;
+    let current = []
+    let y = 0;
+
+    function clickHandler(event){
+        
+        const x = event.clientX;
+        const y = event.clientY;
+        console.log(x);
+        console.log(y);
+
+        for(let i = 0; i < current.length; i++){
+            const objX = current[i].x;
+            const objY = current[i].y;
+            const width = current[i].width;
+            const height = current[i].height;
+            if(x >= objX && x <= (objX + width) && y >= objY && y <= (objY + height)){
+                current.pop();
+                console.log("oiii");
+            }
+        }
+    };
+    
+    function start(){
+        setInterval(function(){
+
+            drawBackground();
+            if(counter < MAX_FRUIT){
+                const obj = new Apple(0,y);
+                y += 60;
+                current.push(obj);
+                counter += 1;
+            }
+            
+            game.addEventListener("click", clickHandler);
+            // Draw all current fruits in action
+            for(let i = 0; i < current.length; i++){
+                current[i].update();
+                current[i].draw();
+            }
+        }
+        ,FPS);
+    }
+
+    start();
 });
