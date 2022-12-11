850d89200678f09fbd1aa36e009d06971d3b0d5c
Lu <lmpa.pt@gmail.com>
2022-12-11 05:40:35 +0100

    drawing apple

--- game.js
+++ game.js
@@ -6,35 +6,44 @@ window.addEventListener('load', function(){
     const HEIGHT = game.height = 500;
     const NO_FRUITS = 3;
     const MAX_FRUIT = 5;
-    
+    const SPEED = 2;
+
     const ctx = game.getContext("2d");
     
     
     const background = new Image();
     background.src = "sky_background.png";
-    
-    let fruitList = []
-    for(let i = 0; i < NO_FRUITS; i++){
-        let fruitImg = new Image();
-        fruitImg.src = "fruit" + String(i + 1) + ".png";
-        fruitList.push(fruitImg);
-    }
 
-    let x = 0
-    let y = 0
-    const orange = new Image();
-    orange.src = "fruit2.png";
+    const appleImg = new Image();
+    appleImg.src = "apple.png";
+    
+    class Apple{
+        constructor(x, y){
+            this.x = x;
+            this.y = y;
+            this.width = 60;
+            this.height = 60;
+        }
+        update(){
+            this.x += SPEED;
+        }
+        draw(){
+            console.log(this.index);
+            ctx.drawImage(appleImg, this.x, this.y, this.width, this.height);
+        }
+    }
     
     function drawBackground(){
         ctx.drawImage(background, 0, 0);
     }
-    function drawOrange(x,y){
-        ctx.drawImage(orange, x, y, 60, 60);
-    }
+
+    const apple = new Apple(0, 0);
+
+
     function move(){
         drawBackground();
-        x += 2;
-        drawOrange(x, y);
+        apple.update();
+        apple.draw();
         requestAnimationFrame(move);
     }
     move();
